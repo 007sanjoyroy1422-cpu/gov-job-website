37c69ac3e6a0116070882f923f40cc2a2639e11e
007sanjoyroy1422-cpu <007sanjoyroy1422@gmail.com>
2025-12-24 18:37:21 +0530

    jobs.js

---
+++ jobs.js
@@ -0,0 +1,14 @@
+const jobs = [
+  {
+    title: "SSC GD Constable",
+    qualification: "10th Pass",
+    lastDate: "30 June 2025",
+    link: "ssc-gd.html"
+  },
+  {
+    title: "Railway Group D",
+    qualification: "10th / ITI",
+    lastDate: "15 July 2025",
+    link: "railway-group-d.html"
+  }
+];
